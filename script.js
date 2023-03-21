ef1319f0c63669ef2c0a016c48fbde070b8950c1
Maryse M <Maryse.Mongeau@cegepoutaouais.qc.ca>
2023-03-21 11:00:17 -0400

    Ajout de commentaires pour expliquer les fonctions ajax dans afficher et ajouter.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function afficher() {
 // GET JSON : Obtenir les données du serveur JSON
     $.getJSON('https://64024cc7302b5d671c37f4b9.mockapi.io/users')
-        .done(function (users) {
-            for (user of users) {
+        // .done() est exécuté quand on a reçu la réponse du serveur
+        .done(function (users) { // users va contenir la liste d'objets json
+            for (user of users) { // user représente chaque objet
                 $("body").append(`
                 <p id='user${user.id}'>${user.id}, ${user.username}, ${user.email}</p>
             `)
@@ -13,6 +14,7 @@ function afficher() {
 $("form").submit(function (){
     //POST : Envoyer des données au serveur JSON
     $.ajax('https://64024cc7302b5d671c37f4b9.mockapi.io/users', {
+        // on envoie le nouvel objet json à ajouter au serveur
         data : JSON.stringify({ "username": $("#username").val(), "email" : $("#email").val() }),
         contentType : 'application/json',
         type : 'POST'
