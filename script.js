2b0e08273d5802e435565266c3507963e4668b64
Maryse M <Maryse.Mongeau@cegepoutaouais.qc.ca>
2023-03-22 15:41:01 -0400

    Ajout de traces de débogage

--- script.js
+++ script.js
@@ -11,14 +11,19 @@ function afficher() {
         });
 }
 
-$("form").submit(function (){
+$("form").submit(function (event){
+    //Tester ce que j'envoie au serveur
+    console.log(JSON.stringify({ "username": $("#username").val(), "email" : $("#email").val() }));
     //POST : Envoyer des données au serveur JSON
     $.ajax('https://64024cc7302b5d671c37f4b9.mockapi.io/users', {
         // on envoie le nouvel objet json à ajouter au serveur
         data : JSON.stringify({ "username": $("#username").val(), "email" : $("#email").val() }),
         contentType : 'application/json',
         type : 'POST'
+    }).catch(error => {
+        console.log(error.message); //Afficher les messages d'erreurs du serveur dans la console.
     });
+    //event.preventDefault(); //On peut ajouter cette ligne temporairement pour s'assurer de voir les traces
 });
 
 function modifier(){
